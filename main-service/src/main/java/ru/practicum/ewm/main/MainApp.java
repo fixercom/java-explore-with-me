92e08ba12abd721fb9012989dba0a25f48a5507e
Alexander Matushin <fixercom@yandex.ru>
2023-04-09 13:45:38 +0300

    fix: change pom files

--- main-service/src/main/java/ru/practicum/ewm/main/MainApp.java
+++ main-service/src/main/java/ru/practicum/ewm/main/MainApp.java
@@ -3,7 +3,7 @@ package ru.practicum.ewm.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"ru.practicum.ewm.main", "ru.practicum.ewm.stats.client"})
+@SpringBootApplication
 public class MainApp {
     public static void main(String[] args) {
         SpringApplication.run(MainApp.class, args);
